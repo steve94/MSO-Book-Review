2ecc6e56276c9e656cb3717ce78b37dbfc59cf9b
Stefan Diesing <stefan.diesing@t-online.de>
2010-12-10 21:36:21 +0100

    scaffold books + haml-rails gem

---
+++ db/migrate/20101210203135_create_books.rb
@@ -0,0 +1,19 @@
+class CreateBooks < ActiveRecord::Migration
+  def self.up
+    create_table :books do |t|
+      t.string :title
+      t.text :description
+      t.date :date_of_publication
+      t.integer :author_id
+      t.integer :publisher_id
+      t.integer :genre_id
+      t.integer :location_id
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :books
+  end
+end
