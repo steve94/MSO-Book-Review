9b3555e7dc61c691547e2e453623813595d337cf
Stefan Diesing <stefan.diesing@t-online.de>
2011-02-20 14:05:55 +0100

    autocomplete & dashboard design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
   
   def auto_complete_input(object, method, title, path) # This are for required fields
-    js_options = {:url => path, :method => :get, :with => "'search=' + element.value"}
+    tag_options = {:class => "string auto_complete required", :size => 50}
+    completion_options = {:url => path, :method => :get, :with => "'search=' + element.value", :skip_style => true}
     
     output = "<div class=\"input string  required\">\n"
     output << "<label class=\"string auto_complete required\">* #{title}</label>"
-    output << text_field_with_auto_complete(object, method,{:class => "string auto_complete required", :size => 50},js_options)
-  
+    output << text_field_with_auto_complete(object, method, tag_options, completion_options)
     output << "</div>"
     return output.html_safe
   end
